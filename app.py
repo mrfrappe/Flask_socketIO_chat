020e96c0a9dbcd7f62cc4e95e7cd91f1fd03a139
Karol Kawski <karol.kawski@protonmail.com>
2018-03-20 13:36:03 +0100

    multi user without names

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
 from time import gmtime, strftime
 
@@ -6,14 +6,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# @socketio.on('my event')
-# def handle_my_custom_event(json):
-#     message = str(json);
-#     print('received json: ' + str(json))
+clients = []
+
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
 
 @socketio.on('message')
-def handle_message(json, broadcast=True):
-    send("(" +str(strftime("%H:%M:%S", gmtime()))+") " + json)
+def handle_message(json):
+    send("(" +str(strftime("%H:%M:%S", gmtime()))+") " + json, broadcast=True)
 
 @app.route("/")
 def main():
